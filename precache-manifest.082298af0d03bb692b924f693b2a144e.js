d08d2a3b88abc6d84489b223d87c34d660a331c5
Vladislav Shevchenko <shev.vlad1991@gmail.com>
2020-04-07 09:31:10 +0300

    Updates

---
+++ precache-manifest.082298af0d03bb692b924f693b2a144e.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44edcfd1e9f12f2ce36315b47fb485ab",
+    "url": "/Acrostia/index.html"
+  },
+  {
+    "revision": "5bd2abb4e624f890db88",
+    "url": "/Acrostia/static/css/2.54cdbe0e.chunk.css"
+  },
+  {
+    "revision": "1b93e04da2fa2052784d",
+    "url": "/Acrostia/static/css/4.7dab1f09.chunk.css"
+  },
+  {
+    "revision": "6c1bdec9919e56211ba3",
+    "url": "/Acrostia/static/css/5.00bb3268.chunk.css"
+  },
+  {
+    "revision": "1ee44cb1a9f33bc07ac5",
+    "url": "/Acrostia/static/css/6.9ec57f2a.chunk.css"
+  },
+  {
+    "revision": "52068cf53aa60a18f2b9",
+    "url": "/Acrostia/static/css/7.2b810bfb.chunk.css"
+  },
+  {
+    "revision": "601fe3a380a16b699663",
+    "url": "/Acrostia/static/css/main.c5d21c31.chunk.css"
+  },
+  {
+    "revision": "5bd2abb4e624f890db88",
+    "url": "/Acrostia/static/js/2.5ee105ad.chunk.js"
+  },
+  {
+    "revision": "0c710714cc0812ad904dc577eda8f829",
+    "url": "/Acrostia/static/js/2.5ee105ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c1b6b2f13fe47b62a0",
+    "url": "/Acrostia/static/js/3.9e35e1e5.chunk.js"
+  },
+  {
+    "revision": "ea0bce3b9c1141e047a7e57907711738",
+    "url": "/Acrostia/static/js/3.9e35e1e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b93e04da2fa2052784d",
+    "url": "/Acrostia/static/js/4.9fd73af4.chunk.js"
+  },
+  {
+    "revision": "6c1bdec9919e56211ba3",
+    "url": "/Acrostia/static/js/5.ab274d02.chunk.js"
+  },
+  {
+    "revision": "1ee44cb1a9f33bc07ac5",
+    "url": "/Acrostia/static/js/6.6f2f3f9b.chunk.js"
+  },
+  {
+    "revision": "52068cf53aa60a18f2b9",
+    "url": "/Acrostia/static/js/7.a11c5414.chunk.js"
+  },
+  {
+    "revision": "601fe3a380a16b699663",
+    "url": "/Acrostia/static/js/main.9d9dccfb.chunk.js"
+  },
+  {
+    "revision": "3c6a5fb74ccbc380f883",
+    "url": "/Acrostia/static/js/runtime-main.d9fe9bc8.js"
+  },
+  {
+    "revision": "57f2ef481e706333c27e35d52ee9de95",
+    "url": "/Acrostia/static/media/about-bg.57f2ef48.png"
+  },
+  {
+    "revision": "d81b952ebceb307cb09549095644afec",
+    "url": "/Acrostia/static/media/arrow-l.d81b952e.svg"
+  },
+  {
+    "revision": "ac61b820c7bc9fe029352175a0b96b30",
+    "url": "/Acrostia/static/media/arrow-r.ac61b820.svg"
+  },
+  {
+    "revision": "70aab7421a04752bc5923e601e846d87",
+    "url": "/Acrostia/static/media/contact-bg.70aab742.png"
+  },
+  {
+    "revision": "381a98d21c4045eebf2c0f4771a03237",
+    "url": "/Acrostia/static/media/giars.381a98d2.svg"
+  },
+  {
+    "revision": "a48369c7ca0f4768dd0bc709a8e79da0",
+    "url": "/Acrostia/static/media/home-bg.a48369c7.png"
+  },
+  {
+    "revision": "7488463e6effd29cc8b2251b2fa68ae1",
+    "url": "/Acrostia/static/media/lightball.7488463e.svg"
+  },
+  {
+    "revision": "aa2274728d71106f55b40d38c82dc07f",
+    "url": "/Acrostia/static/media/map.aa227472.jpg"
+  },
+  {
+    "revision": "135fcfc85c34a7c6b98577e0270f2763",
+    "url": "/Acrostia/static/media/rocket.135fcfc8.svg"
+  },
+  {
+    "revision": "506e47cb82637a333645e948ec2ccee5",
+    "url": "/Acrostia/static/media/services-bg.506e47cb.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Acrostia/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Acrostia/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Acrostia/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Acrostia/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ee77691db3618c95b02928cdf3b93b07",
+    "url": "/Acrostia/static/media/w1.ee77691d.jpg"
+  },
+  {
+    "revision": "7f15ea953092424819a37ccd2213611e",
+    "url": "/Acrostia/static/media/w2.7f15ea95.jpg"
+  },
+  {
+    "revision": "1b27dff1899b4d230e7b41d25aac52db",
+    "url": "/Acrostia/static/media/w3.1b27dff1.jpg"
+  },
+  {
+    "revision": "9f0d7c7d019a46315342f76c10b1531d",
+    "url": "/Acrostia/static/media/w4.9f0d7c7d.jpg"
+  },
+  {
+    "revision": "57313e15b0aa4e914f81fdcefa3b2e94",
+    "url": "/Acrostia/static/media/w5.57313e15.jpg"
+  },
+  {
+    "revision": "89c8c5dc690ad91ffaa52899d71059fb",
+    "url": "/Acrostia/static/media/w6.89c8c5dc.jpg"
+  },
+  {
+    "revision": "a3cc5bf594614ae683b0d5a24c4fc711",
+    "url": "/Acrostia/static/media/w7.a3cc5bf5.jpg"
+  },
+  {
+    "revision": "192a444a0ad273edc80725ab45deb8f3",
+    "url": "/Acrostia/static/media/w8.192a444a.jpg"
+  },
+  {
+    "revision": "70753fa6454c4b67c4e84e0a1db94ea8",
+    "url": "/Acrostia/static/media/wand.70753fa6.svg"
+  },
+  {
+    "revision": "a51f2cefb0ec7e0447a6b44e793a084c",
+    "url": "/Acrostia/static/media/work-bg.a51f2cef.png"
+  }
+]);
